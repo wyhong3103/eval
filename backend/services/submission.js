0e0b9c809e5845c847d947d8d3850d014c17958e
wyhong3103 <wyhong3103@gmail.com>
2024-07-09 23:53:38 +0800

    add cors

--- backend/services/submission.js
+++ backend/services/submission.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
-const io = require("socket.io")();
+const io = require("socket.io")({
+  cors: {
+    origin: "*",
+  },
+});
 const amqp = require("amqplib/callback_api");
 const RABBITMQ_URL = process.env.RABBITMQ_URL;
 const task_queue = process.env.QUEUE_NAME;
@@ -21,22 +25,29 @@ amqp.connect(RABBITMQ_URL, (err, connection) => {
     channel.assertQueue(task_queue, { durable: true });
     channel.assertQueue(rpc_queue, { durable: true });
 
-    channel.consume(rpc_queue, (msg) => {
-      const { id, output, status } = JSON.parse(msg.content.toString());
-      if (clients[id]) {
-        clients[id].emit("result", output, status);
+    channel.consume(
+      rpc_queue,
+      (msg) => {
+        const { id, output, status } = JSON.parse(msg.content.toString());
+        console.log(`${id}: Code evaluated.`);
+        if (clients[id]) {
+          clients[id].emit("result", output, status);
+        }
+      },
+      {
+        noAck: true,
       }
-    });
+    );
   });
 });
 
 io.on("connection", (socket) => {
-  console.log(`${socket.id} has connected!`);
   clients[socket.id] = socket;
   socket.on("submit", (msg) => {
     msg = JSON.parse(msg);
     msg.id = socket.id;
     msg = JSON.stringify(msg);
+    console.log(`${socket.id}: Code submitted.`);
     channel.sendToQueue(task_queue, Buffer.from(msg));
   });
 
